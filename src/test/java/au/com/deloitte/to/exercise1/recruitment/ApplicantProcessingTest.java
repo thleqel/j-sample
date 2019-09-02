87296ee24d98d8e382d9d2e76a8db91c01db9c65
Thanh Le <thle@deloitte.com.au>
2019-09-02 14:45:39 +1000

    add tests

---
+++ src/test/java/au/com/deloitte/to/exercise1/recruitment/ApplicantProcessingTest.java
@@ -0,0 +1,28 @@
+package au.com.deloitte.to.exercise1.recruitment;
+
+import au.com.deloitte.to.exercise1.model.Applicant;
+import au.com.deloitte.to.exercise1.model.Nationality;
+import au.com.deloitte.to.exercise1.model.ResidentStatus;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Thanh Le
+ * Created: 02 Sep 2019
+ */
+public class ApplicantProcessingTest {
+
+    @Test
+    public void testEligibleApplicant(){
+        Applicant applicant = new Applicant();
+        applicant.setFirstNames("First Name");
+        applicant.setLastName("Last Name");
+        applicant.setAge(30);
+        applicant.setNationality(Nationality.AUSTRALIA);
+        applicant.setResidentStatus(ResidentStatus.PERMANENT_RESIDENT);
+        ApplicantProcessing applicantProcessing = new ApplicantProcessing(applicant);
+        applicantProcessing.process();
+        assertEquals("Proceed Interview", applicant.getStatus());
+    }
+}
